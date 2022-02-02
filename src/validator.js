02dda641670703d3f0a27c17deeff5f03985e3a2
pasadem <pasadem@ukr.net>
2022-02-02 15:56:27 +0200

    tests

--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,10 @@
 import * as yup from 'yup';
 
-const validator = (url, feeds) => {
+const validator = (urlVal, feeds) => {
   const schema = yup.string()
     .required('empty')
     .url('invalidUrl')
-    .notOneOf(feeds.map(({ link }) => url), 'duplicate');
-  return schema.validate(url);
+    .notOneOf(feeds.map(({ url }) => url), 'duplicate');
+  return schema.validate(urlVal);
 };
 export default validator;
